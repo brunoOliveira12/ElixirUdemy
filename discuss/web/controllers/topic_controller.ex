e579622835a673834ec993c2491994a2724a1fb4
brunooliveira <brunooliveira@debian.debian>
2021-02-16 09:09:14 -0300

    phoenix mvc

---
+++ discuss/web/controllers/topic_controller.ex
@@ -0,0 +1,12 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+  def new(conn, _params) do
+    changeset = Discuss.Topic.changeset(%Discuss.Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+
+  end
+end
